656a0b18256a906ba11a638298800468157f9bd4
carlos marin <carlosmarind@gmail.com>
2023-11-02 19:43:28 -0300

    segundo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { publicKey } from './api'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  fetch(`https://gateway.marvel.com//v1/public/characters?name=hulk&apikey=${publicKey}`, {
+    method: 'GET',
+  }).then(response => {
+
+    if (response.ok) {
+      return response.json() as Promise<CharacterDataWrapper>;
+    } else {
+      throw new Error('La solicitud no se pudo completar');
+    }
+
+
+  }).then(json => {
+    console.log(json);
+  }).catch(error => {
+
+    console.log(error);
+
+  })
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
