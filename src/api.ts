92e427e13c51ab597a69f2d71d0de4df173e4633
carlosmarind <carlosmarind@gmail.com>
2023-11-08 18:33:48 -0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,2 @@
-export const publicKey = "b95769350686f0233d69c4aa0832f9a4";
-export const privateKey = "8b64ce0bb78b6a2558adf967d4c44a5b864ce9fd";
+export const publicKey = "";
+export const privateKey = "";
